d58e60476a5bf0f5bd969d803f5fd99e7b9c8199
Sagar <kumarsagar15795@gmail.com>
2016-09-23 09:26:26 +0530

    add jokes in java library

--- jokeprovider/src/main/java/com/example/MyJokes.java
+++ jokeprovider/src/main/java/com/example/MyJokes.java
@@ -1,6 +1,33 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class MyJokes {
 
+    private final int totalJokes;
+    private ArrayList<String> jokes = new ArrayList<String>();
+    public MyJokes() {
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
+        jokes.add("Funny Joke");
 
+        totalJokes = jokes.size();
+    }
+
+    public String getMeJoke(){
+        int rand = (int) Math.ceil((totalJokes - 1) * Math.random());
+        return jokes.get(rand);
+    }
 }
