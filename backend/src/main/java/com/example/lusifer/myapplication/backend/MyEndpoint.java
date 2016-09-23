55e7bb9ed1b2788833ab44dd54293013cf355f13
Sagar <kumarsagar15795@gmail.com>
2016-09-23 11:34:55 +0530

    add jokeprovider in GCM module

--- backend/src/main/java/com/example/lusifer/myapplication/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/lusifer/myapplication/backend/MyEndpoint.java
@@ -6,6 +6,7 @@
 
 package com.example.lusifer.myapplication.backend;
 
+import com.example.MyJokes;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -28,7 +29,7 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData(new MyJokes().getMeJoke());
 
         return response;
     }
